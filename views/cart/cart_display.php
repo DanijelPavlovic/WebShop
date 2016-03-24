4386227de561c08ae7a6def2d8a1e684a0a30943
Danijel <danci1159@gmail.com>
2016-03-24 12:01:08 +0100

    Added files via upload

---
+++ views/cart/cart_display.php
@@ -0,0 +1,79 @@
+
+        <div class="container">
+                <div class="col-sm-6 padding-right">
+                        <div class="features_items"><!--features_items-->
+                                <h2 class="title text-center">Cart Items</h2>
+                                        <?php echo form_open('shoppingcart/update'); ?>
+
+                                        <table cellpadding="12" cellspacing="1"  border="1">
+                                        
+                                                <tr>
+                                                        <th>Item ID</th>
+                                                        <th>Item Description</th>
+                                                        <th style="text-align:right">Item Price</th>
+                                                        <th>Quantity</th>
+                                                        <th style="text-align:right">Sub-Total</th>
+                                                        <th align="center">Remove</th>  
+                                                </tr>
+
+                                                <?php $i = 1; ?>
+
+                                                <?php foreach ($this->cart->contents() as $items): ?>
+
+                                                <?php echo form_hidden($i.'[rowid]', $items['rowid']); ?>
+
+                                                <tr>    
+                                                        <!-- Display Item ID -->
+                                                        <td><?php echo $items['id']; ?></td>
+
+                                                        <!-- Display Item Description -->
+                                                         <td>
+                                                                <?php echo $items['name']; ?>
+
+                                                                <?php if ($this->cart->has_options($items['rowid']) == TRUE): ?>
+
+                                                                        <p>
+                                                                                <?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value): ?>
+
+                                                                                        <strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
+
+                                                                                <?php endforeach; ?>
+                                                                        </p>
+
+                                                                <?php endif; ?>
+                                                        </td>
+                                                        <!-- Display Item Price -->
+                                                        <td style="text-align:right"><?php echo $this->cart->format_number($items['price']); ?></td>
+
+                                                        <!-- Display Item Quantity -->
+                                                        <td><?php echo form_input(array('name' => 'qty'.$i, 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
+
+                                                        <!-- Display Item Sub-Total -->
+                                                        <td style="text-align:right">$<?php echo $this->cart->format_number($items['subtotal']); ?></td>
+
+                                                        <!-- Remove item-->
+                                                        <td><?php echo anchor('shoppingcart/delete/'.$items['rowid'], 'Remove product'); ?></td>    
+                                                </tr>
+
+
+                                                <?php $i++; ?>
+
+                                                <?php endforeach; ?>
+
+                                                <tr>
+                                                        <td colspan="3"> </td>
+                                                        <td class="right"><strong>Total</strong></td>
+                                                        <td class="right">$<?php echo $this->cart->format_number($this->cart->total()); ?></td>
+                                                        <td></td>
+                                                </tr>
+                                        </table>
+                                        <?php form_close(); ?>
+
+                        </div>
+                </div><!--col-sm-9-->
+        </div>
+
+
+
+
+
---
+++ views/cart/cart_view.php
@@ -0,0 +1,30 @@
+
+<!-- Shoping cart display -->
+<section>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 padding-right">
+            <div class="features_items"><!--features_items-->
+                <h2 class="title text-center">Items in Cart</h2>
+                <h2 class="title text-left">No Item's in cart </h2>
+
+<?php
+/*
+echo '<br>';
+$this->load->library('table');
+$this->table->set_heading('Code','Name','Price', 'Quantity', 'Buy');
+foreach($listProducts as $p)
+$this->table->add_row($p->id_products, $p->product_name, $p->price, $p->stock, anchor('shoppingcart/buy/'.$p->id_products, 'Order Now'));
+$this->table->set_template(array('table_open' => '<table border="1" cellpadding="3" cellspacing="3" >'));
+
+
+echo $this->table->generate();
+*/
+?>
+
+
+
+            </div>
+    </div><!--col-sm-9-->
+</div>
+</section>
